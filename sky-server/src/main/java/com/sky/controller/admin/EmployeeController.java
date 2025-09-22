b8167edc610fe466f40029745199f7d22e22de87
hello <13719774388@163.com>
2025-09-22 11:21:05 +0800

    新增员工业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -70,6 +71,14 @@ public class EmployeeController {
      *
      * @return
      */
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工信息:{}",employeeDTO);
+        employeeService.save(employeeDTO);
+        return Result.success();
+    }
+
     @ApiOperation("登出操作")
     @PostMapping("/logout")
     public Result<String> logout() {
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -22,6 +25,23 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.error("异常信息:{}",ex.getMessage());
+        String message = ex.getMessage();
+        //键值重复
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            //重复的键值
+            String duplicateProperty=split[2];
+            //错误信息
+            return Result.error(duplicateProperty+ MessageConstant.ALREADY_EXISTS);
+        }else {
+            //未知错误
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
